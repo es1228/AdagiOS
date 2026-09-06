3912b7c6c154494d6e58d4edaced2dee6b751d36
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-09-06 01:13:56 -0400

    fix: add coep for all iframes

--- src/apps/SatelliteApp.tsx
+++ src/apps/SatelliteApp.tsx
@@ -1,25 +1,38 @@
 import Draggable from "react-draggable";
 import type { AppProps } from "../App";
 import Titlebar from "../components/Titlebar";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const SatelliteApp = ({ isOpen, isOnTop, toggleClose, onClick }: AppProps) => {
-    const nodeRef = useRef(null);
+	const nodeRef = useRef(null);
 
-    if (!isOpen) return null;
+	const iFrameRef = useRef(null);
 
-    return (
-        <Draggable nodeRef={nodeRef} cancel=".non-draggable">
-            <div
-                ref={nodeRef}
-                className="bg-on-bg/20 dark:bg-on-bg-dark/40 fixed top-1/2 left-1/2 h-150 w-300 -translate-x-1/2 -translate-y-1/2 overflow-auto rounded-3xl p-4 backdrop-blur-3xl"
-                style={{ zIndex: isOnTop ? 50 : 10 }}
-                onClick={onClick}
-            >
-                <Titlebar windowName="Satellite Tracker" handleClose={toggleClose} />
-                <iframe className="h-9/10 w-full rounded-3xl" src="https://es1228.github.io/SatTrack" allow="cross-origin-isolated"></iframe>
-            </div>
-        </Draggable>
-    );
+	useEffect(() => {
+		if (iFrameRef.current) (iFrameRef.current as any).credentialless = true;
+	}, []);
+
+	if (!isOpen) return null;
+
+	return (
+		<Draggable nodeRef={nodeRef} cancel=".non-draggable">
+			<div
+				ref={nodeRef}
+				className="bg-on-bg/20 dark:bg-on-bg-dark/40 fixed top-1/2 left-1/2 h-150 w-300 -translate-x-1/2 -translate-y-1/2 overflow-auto rounded-3xl p-4 backdrop-blur-3xl"
+				style={{ zIndex: isOnTop ? 50 : 10 }}
+				onClick={onClick}
+			>
+				<Titlebar
+					windowName="Satellite Tracker"
+					handleClose={toggleClose}
+				/>
+				<iframe
+					ref={iFrameRef}
+					className="h-9/10 w-full rounded-3xl"
+					src="https://es1228.github.io/SatTrack"
+				></iframe>
+			</div>
+		</Draggable>
+	);
 };
 export default SatelliteApp;
--- src/apps/WeatherApp.tsx
+++ src/apps/WeatherApp.tsx
@@ -1,25 +1,35 @@
 import Draggable from "react-draggable";
 import type { AppProps } from "../App";
 import Titlebar from "../components/Titlebar";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const WeatherApp = ({ isOpen, isOnTop, toggleClose, onClick }: AppProps) => {
-    const nodeRef = useRef(null);
+	const nodeRef = useRef(null);
 
-    if (!isOpen) return null;
+	const iFrameRef = useRef(null);
 
-    return (
-        <Draggable nodeRef={nodeRef} cancel=".non-draggable">
-            <div
-                ref={nodeRef}
-                className="bg-on-bg/20 dark:bg-on-bg-dark/40 fixed top-1/2 left-1/2 h-150 w-100 -translate-x-1/2 -translate-y-1/2 resize overflow-auto rounded-3xl p-4 backdrop-blur-3xl"
-                style={{ zIndex: isOnTop ? 50 : 10 }}
-                onClick={onClick}
-            >
-                <Titlebar windowName="Weather" handleClose={toggleClose} />
-                <iframe className="h-9/10 w-full rounded-3xl" src="https://es1228.github.io/OpenSky" allow="cross-origin-isolated"></iframe>
-            </div>
-        </Draggable>
-    );
+	useEffect(() => {
+		if (iFrameRef.current) (iFrameRef.current as any).credentialless = true;
+	}, []);
+
+	if (!isOpen) return null;
+
+	return (
+		<Draggable nodeRef={nodeRef} cancel=".non-draggable">
+			<div
+				ref={nodeRef}
+				className="bg-on-bg/20 dark:bg-on-bg-dark/40 fixed top-1/2 left-1/2 h-150 w-100 -translate-x-1/2 -translate-y-1/2 resize overflow-auto rounded-3xl p-4 backdrop-blur-3xl"
+				style={{ zIndex: isOnTop ? 50 : 10 }}
+				onClick={onClick}
+			>
+				<Titlebar windowName="Weather" handleClose={toggleClose} />
+				<iframe
+					ref={iFrameRef}
+					className="h-9/10 w-full rounded-3xl"
+					src="https://es1228.github.io/OpenSky"
+				></iframe>
+			</div>
+		</Draggable>
+	);
 };
 export default WeatherApp;
